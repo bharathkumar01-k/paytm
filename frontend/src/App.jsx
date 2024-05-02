04eb57628dc5347559e7d46d83464dff633253c9
bharathkumar01-k <42500502+bharathkumar01-k@users.noreply.github.com>
2024-05-03 00:16:20 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import { SigninComponent } from "./components/SigninComponent";
 import { SignupComponent } from "./components/SignupComponent";
 import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
     return (
         <BrowserRouter>
